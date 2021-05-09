c858761719e9cf9c52aac5e332bc004788bd82a9
Lei Qin <lineslk97@gmail.com>
2021-05-09 19:09:21 +1000

    Reconstructed Nonlinear MPC
    Changed nonlinearMPC.m to main.m. Delete useless .mat and .m files. Reformated the codes.

--- Optimal Path Following/Nonlinear MPC/main.m
+++ Optimal Path Following/Nonlinear MPC/main.m
@@ -3,18 +3,29 @@ clear all;
 close all;
 
 %% Basic setting
-% Load path, outer boundary and center of track as xPath, yPath, yCenter, 
-% xInner, yInner, xOuter, yOuter
-loadTrackAndPredPath;  
-xRef = xPath;
-yRef = yPath;
+% Load path, outer boundary and center of track.
+load('track.mat');
 
-thetaRef = zeros(1, length(xPath));
+% Track information
+xPath = track.center(1,:);
+yPath = track.center(2,:);
+xCenter = track.center(1,:);
+yCenter = track.center(2,:);
+xInner = track.inner(1,:);
+yInner = track.inner(2,:);
+xOuter = track.outer(1,:);
+yOuter = track.outer(2,:);
 centerPoints = [xCenter; yCenter];
 innerPoints = [xInner; yInner];
 outerPoints = [xOuter; yOuter];
-xiRef = [xPath; yPath; zeros(2, length(xRef))];
-% Edge of track
+
+% Reference
+xRef = xPath;
+yRef = yPath;
+thetaRef = zeros(1, length(xPath));
+xiRef = [xPath; yPath; zeros(1, length(xPath)); zeros(1, length(xPath))];
+
+% Bound constraint of track
 xTrackMin = min([xInner, xOuter]);
 xTrackMax = max([xInner, xOuter]);
 yTrackMin = min([yInner, yOuter]);
@@ -27,6 +38,7 @@ flagPlot = true;
 %% Constraints
 % Specify system constraints
 constr = struct;
+
 % State constraints
 % Note that x-y position constraints cannot be set here since they are 
 % constrained by the track boundaries in the controller
@@ -45,7 +57,7 @@ Q = 20*diag([1 1 0 0]); % [x weight, y weight, theta weight, v weight]
 R = 5*diag([1 1]); % [omega weight, a weight]
 
 %% MPC setting
-N = 15;  % length of horizon
+N = 20;  % length of horizon
 M = 1;  % number of iterations to repeat LTV for single time
 T = 0.05;  % aampling period
 tspan = [0 0];  % loop through times
@@ -53,7 +65,7 @@ iterMax = 50; % Max iterations for IPOPT solver
 
 % Set simulation time
 startTime = 0;
-endTime = T*(length(xRef)-N-1);
+endTime = T*(length(xRef)-1);
 times = startTime: T: endTime;
 
 % Constant terms
@@ -80,26 +92,32 @@ controller = NMPC(N, iterMax, T ,Q, R, constr);
 uHis = []; % History of implemented inputs
 xiHis = xi; % History of implemented states
 epsSim = []; % Record array of epsilons, corresponding to norm of 
-
-% Loop through times for simulation
 tspan = [0 0];
+
+% Fill references and center points
+xiRef = [xiRef, xiRef(:, 1:N)];
+centerPoints = [centerPoints, centerPoints(:, 1:N)];
+
 % fprintf('Simulation Progress:         ')
-% difference between inputs in current and previous iteration. Indicates
-% the convergence of inputs in an iteration.
+
 record = struct;
 record.refs = cell(1,length(times));
 record.xiHis = zeros(nState,N+1);
 record.uPred = cell(1,length(times));
 record.uHis = zeros(nInput,N+1);
 
-for k = 1:length(times) % starting off at first time already (not zeroth)
+for k = 1: length(times) % starting off at first time already (not zeroth)
     % fprintf('\b\b\b\b\b\b\b\b%6.2f %%',(i/(length(TIME)-1)*100));
-    % Add current refs to ltv_reqs
+    
+    % Add current references
     record.refs{k} = xiRef(:, k:k+N);
-    % Add input guesses to ltv_reqs when k>=2
+    
+    % Add predicted inputs when k>=2
     if (k >= 2)
         record.uPred{k} = [uPred(:,2:end) uPred(:,end)];
     end
+    
+    % Slove noncovex optimisation problems
     solutions = controller{xiRef(:,k:k+N), xi, u};
     uPred = solutions{1};
     xiPred = solutions{2};
@@ -109,7 +127,7 @@ for k = 1:length(times) % starting off at first time already (not zeroth)
         record.uPred{k} = uPred(:,:);
     end
     
-    % Apply input to plant
+    % Calculate current states with a ODE model
     tspan = [tspan(2) tspan(2)+T];
     [t,xiODE] = ode45(@(t, xiODE) unicycleODE(t,xiODE, ...
                 uPred(1:nInput,2)), tspan, xi(1:nState,1));
@@ -120,32 +138,38 @@ for k = 1:length(times) % starting off at first time already (not zeroth)
     uHis = [uHis, uPred(:,2)];
     xiHis = [xiHis,xi];
     
-    % Plot position inside simulation
+    % Plot position states inside simulation
     if flagPlot
-        % Plot solved positions from MPC along horizon
+        % Plot predicted positions from MPC along horizon
         plot(xiPred(1,1:size(xiPred,2)), ...
-             xiPred(2,1:size(xiPred,2)), 'LineWidth', 1.5)
+                xiPred(2,1:size(xiPred,2)), 'LineWidth', 1.5)
+         
         hold on 
-
-        % Set axis for plot to track edges
+        
+        % Plot references
+        plot(xiRef(1,k+1:k+N),xiRef(2,k+1:k+N))
+        
+        % Set axis to track edges
         axis([xTrackMin xTrackMax yTrackMin yTrackMax])
 
         % Plot track boundaries
         plot(xOuter,yOuter,'k')
         plot(xInner,yInner,'k')
-        % Plot references
-        plot(xiRef(1,k+1:k+N),xiRef(2,k+1:k+N))
-
-        % Plot history of positions from t=0 to current t
-        % scatter(xiHis(1,:),xiHis(2,:))
         
+        % Plot history of positions from t=0 to current t        
         plotTrVec = [xi(1:2); 0];
         plotRot = axang2quat([0 0 1 xi(3)]);
-        plotTransforms(plotTrVec', plotRot, "MeshFilePath", "groundvehicle.stl", "Parent", gca, "View","2D", "FrameSize", 0.15);
+        plotTransforms(plotTrVec', plotRot, "MeshFilePath", ...
+                       "groundvehicle.stl", "Parent", gca, "View",...
+                       "2D", "FrameSize", 0.15);
         light;
         
+        % scatter(xiHis(1,:), xiHis(2,:))
+        
         hold off
-        % legend('Reference track', 'Current pose')
+        
+        legend('Predicted pose', 'Reference track')
+        
         pause(1e-6)
     end
     
@@ -159,9 +183,57 @@ eps_avg = mean(epsSim);
 fprintf('\n')
 
 %% Plot System, States and Inputs
-plotPositions
-plotStates
-plotInputs
+% Plot history of positions during simulation as well as track
+figure
+plot(xiHis(1,:),xiHis(2,:))
+hold on
+% axis([track_xmin track_xmax track_ymin track_ymax])
+plot(xCenter,yCenter,'r')
+plot(xOuter,yOuter,'k')
+plot(xInner,yInner,'k')
+title('Positions')
+xlabel('X')
+ylabel('Y')
+legend('vehicle','reference', 'boundary')
+
+% Plot history of states during simulation
+% Plot x history
+figure
+subplot(4,1,1)
+plot(times(1:end), xiHis(1,1:end-1))
+title('States')
+ylabel("x [m]")
+grid on
+% Plot y history
+subplot(4,1,2)
+plot(times(1:length(times)), xiHis(2,1:end-1))
+ylabel("y [m]")
+grid on
+% Plot v history
+subplot(4,1,3)
+plot(times(1:length(times)), xiHis(3,1:end-1))
+ylabel("\theta (\circ)")
+grid on
+% Plot theta history
+subplot(4,1,4)
+plot(times(1:length(times)), xiHis(4,1:end-1))
+grid on
+ylabel("v [m/s]")
+
+% Plot history of inputs during simulation
+figure
+% Plot omega history
+subplot(2,1,1)
+stairs(times(1:length(times)), uHis(1,:))
+title('Inputs')
+ylabel("\omega [rad/s]")
+grid on
+% Plot a history
+subplot(2,1,2)
+stairs(times(1:length(times)), uHis(2,:))
+grid on
+ylabel("a [m/s^2]")
+xlabel("Time (s)")
 
 %% Save data for analyis
 record = struct;
